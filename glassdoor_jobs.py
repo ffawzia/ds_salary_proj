61f20ed80632c2a9249992ea6282f2da8ac542e2
ffawzia <60164280+ffawzia@users.noreply.github.com>
2020-09-21 18:26:05 +0300

    Uploaded scraper and run code

---
+++ glassdoor_jobs.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 00:14:26 2020
+
+@author: ffawz
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path= 'C:/Users/ffawz/Documents/ds_salary_proj/chromedriver'
+df = gs.get_jobs('data scientist',70, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
